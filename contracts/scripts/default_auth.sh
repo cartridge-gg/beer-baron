ba760f6a0188c74e7a6047a77229c41d8c30867b
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-11-20 21:11:39 +0100

    auth

--- contracts/scripts/default_auth.sh
+++ contracts/scripts/default_auth.sh
@@ -2,17 +2,50 @@
 set -euo pipefail
 pushd $(dirname "$0")/..
 
-export WORLD_ADDRESS="0x7d1f066a910bd86f532fa9ca66766722c20d47462fb99fb2fb0e1030262f9c5";
+# export RPC_URL="http://localhost:5050";
+
+export RPC_URL="https://api.cartridge.gg/x/beer-baron-prod-2/katana"
+
+export WORLD_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.world.address')
+
+export AUCTIONS_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "auctions" ).address')
+export BREWING_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "brewing" ).address')
+export FARMING_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "farming" ).address')
+export LOBBY_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "lobby" ).address')
+export TRADING_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "trading" ).address')
+
+echo "---------------------------------------------------------------------------"
+echo world : $WORLD_ADDRESS 
+echo " "
+echo actions : $AUCTIONS_ADDRESS
+echo "---------------------------------------------------------------------------"
 
 # enable system -> component authorizations
-COMPONENTS=("Position" "Moves" )
+COMPONENTS=($(cat "./target/dev/manifest.json" | jq -r '.models[].name' | sed 's/.*/"&"/'))
 
 for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component spawn --world $WORLD_ADDRESS
+    sozo auth writer $component $AUCTIONS_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sleep 1 
 done
 
 for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component move --world $WORLD_ADDRESS
+    sozo auth writer $component $BREWING_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sleep 1 
+done
+
+for component in ${COMPONENTS[@]}; do
+    sozo auth writer $component $FARMING_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sleep 1 
+done
+
+for component in ${COMPONENTS[@]}; do
+    sozo auth writer $component $LOBBY_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sleep 1 
+done
+
+for component in ${COMPONENTS[@]}; do
+    sozo auth writer $component $TRADING_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sleep 1 
 done
 
 echo "Default authorizations have been successfully set."
