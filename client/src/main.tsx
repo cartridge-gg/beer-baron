0dbdabd93b600b0db8f6e997161232a2991f6177
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-02-08 17:32:55 +1100

    update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import { setup } from './dojo/generated/setup.ts';
 import { DojoProvider } from './dojo/DojoContext.tsx';
-import { dojoConfig } from '../dojoConfig.ts';
+import { dojoConfig } from './dojoConfig.ts';
 
 async function init() {
     const rootElement = document.getElementById('root');
